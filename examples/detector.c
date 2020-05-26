7398765e8c5865cefbb44828cb63b9ce1d9f3e19
jdf <987293233@qq.com>
2020-05-26 16:52:37 +0800

    增加批量预测图片代码
    增加批量预测图片代码
    使用方法：
    测试单张图片images/*.jpg，生成目录在images_out/*_out.jpg
    ```
    ./darknet detector  test cfg/*.data cfg/*.cfg backup/*.weights images/*.jpg   -out images_out
    
    ```
    
    测试目录images下的所有图片，生成图片在images_out下*_out.jpg
    ```
    ./darknet detector  test cfg/*.data cfg/*.cfg backup/*.weights images   -out images_out
    ```

--- examples/detector.c
+++ examples/detector.c
@@ -1,4 +1,8 @@
 #include "darknet.h"
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
 
 static int coco_ids[] = {1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,22,23,24,25,27,28,31,32,33,34,35,36,37,38,39,40,41,42,43,44,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,67,70,72,73,74,75,76,77,78,79,80,81,82,84,85,86,87,88,89,90};
 
@@ -559,7 +563,106 @@ void validate_detector_recall(char *cfgfile, char *weightfile)
 }
 
 
-void test_detector(char *datacfg, char *cfgfile, char *weightfile, char *filename, float thresh, float hier_thresh, char *outfile, int fullscreen)
+
+
+
+
+void test_detector_image(char *filename, char *outfile, network * net,
+			 float thresh, float hier_thresh, char **names,
+			 image ** alphabet)
+{
+    double time;
+    char buff[256];
+    char *input = buff;
+    float nms = .45;
+    while (1) {
+	if (filename) {
+	    strncpy(input, filename, 256);
+	} else {
+	    printf("Enter Image Path: ");
+	    fflush(stdout);
+	    input = fgets(input, 256, stdin);
+	    if (!input)
+		return;
+	    strtok(input, "\n");
+	}
+	printf("to load_image_color: %s  \n", input);
+	image im = load_image_color(input, 0, 0);
+	image sized = letterbox_image(im, net->w, net->h);
+	layer l = net->layers[net->n - 1];
+
+
+	float *X = sized.data;
+	time = what_time_is_it_now();
+	network_predict(net, X);
+	printf("%s: Predicted in %f seconds.\n", input,
+	       what_time_is_it_now() - time);
+	int nboxes = 0;
+	detection *dets =
+	    get_network_boxes(net, im.w, im.h, thresh, hier_thresh,
+			      0, 1, &nboxes);
+	//printf("%d\n", nboxes);
+	//if (nms) do_nms_obj(boxes, probs, l.w*l.h*l.n, l.classes, nms);
+	if (nms)
+	    do_nms_sort(dets, nboxes, l.classes, nms);
+        //printf("--------------------------");
+	draw_detections(im, dets, nboxes, thresh, names, alphabet,
+			l.classes);
+	free_detections(dets, nboxes);
+	if (outfile) {
+            char outfilepath[255];
+	    struct stat st;
+	    stat(outfile, &st);
+            //get last save result filename. example:(smoke1_out.jpg)
+	    if (S_ISDIR(st.st_mode)) {
+                char *tempFileName;
+                tempFileName=strdup(filename);
+		char *token;
+		char *filesimpleName;
+		int i = 0;
+		for (token = strsep(&tempFileName, "/"); token != NULL;
+		     token = strsep(&tempFileName, "/")) {
+		    i++;
+		    if (i == 2) {
+			//printf("token  %s\n", token);
+			filesimpleName = strsep(&token, ".");
+			if (filesimpleName != NULL) {
+			    memset(outfilepath, 0, sizeof(outfilepath));
+                            strcat(outfilepath, outfile);
+			    strcat(outfilepath, "/");
+                            strcat(outfilepath, filesimpleName);
+                            strcat(outfilepath, "_out");
+			}
+			//printf("outfile  %s\n", outfilepath);
+			break;
+		    }
+		}
+                free(tempFileName);
+		
+	    }
+            printf("outfile: %s\n\n", outfilepath);
+	    save_image(im, outfilepath);
+            memset(outfilepath,0x0,255);
+	} else {
+	    save_image(im, "predictions");
+#ifdef OPENCV
+	    make_window("predictions", 512, 512, 0);
+	    show_image(im, "predictions", 0);
+#endif
+	}
+
+	free_image(im);
+	free_image(sized);
+	if (filename)
+	    break;
+    }
+
+}
+
+
+void test_detector(char *datacfg, char *cfgfile, char *weightfile,
+		   char *filename, float thresh, float hier_thresh,
+		   char *outfile, int fullscreen)
 {
     list *options = read_data_cfg(datacfg);
     char *name_list = option_find_str(options, "names", "data/names.list");
@@ -569,56 +672,46 @@ void test_detector(char *datacfg, char *cfgfile, char *weightfile, char *filenam
     network *net = load_network(cfgfile, weightfile, 0);
     set_batch_network(net, 1);
     srand(2222222);
-    double time;
-    char buff[256];
-    char *input = buff;
-    float nms=.45;
-    while(1){
-        if(filename){
-            strncpy(input, filename, 256);
-        } else {
-            printf("Enter Image Path: ");
-            fflush(stdout);
-            input = fgets(input, 256, stdin);
-            if(!input) return;
-            strtok(input, "\n");
-        }
-        image im = load_image_color(input,0,0);
-        image sized = letterbox_image(im, net->w, net->h);
-        //image sized = resize_image(im, net->w, net->h);
-        //image sized2 = resize_max(im, net->w);
-        //image sized = crop_image(sized2, -((net->w - sized2.w)/2), -((net->h - sized2.h)/2), net->w, net->h);
-        //resize_network(net, sized.w, sized.h);
-        layer l = net->layers[net->n-1];
+    printf("-------start detector------- \n");
+    printf("image name or dir: %s \n", filename);
+    printf("out file name or dir: %s \n", outfile);
 
 
-        float *X = sized.data;
-        time=what_time_is_it_now();
-        network_predict(net, X);
-        printf("%s: Predicted in %f seconds.\n", input, what_time_is_it_now()-time);
-        int nboxes = 0;
-        detection *dets = get_network_boxes(net, im.w, im.h, thresh, hier_thresh, 0, 1, &nboxes);
-        //printf("%d\n", nboxes);
-        //if (nms) do_nms_obj(boxes, probs, l.w*l.h*l.n, l.classes, nms);
-        if (nms) do_nms_sort(dets, nboxes, l.classes, nms);
-        draw_detections(im, dets, nboxes, thresh, names, alphabet, l.classes);
-        free_detections(dets, nboxes);
-        if(outfile){
-            save_image(im, outfile);
-        }
-        else{
-            save_image(im, "predictions");
-#ifdef OPENCV
-            make_window("predictions", 512, 512, 0);
-            show_image(im, "predictions", 0);
-#endif
-        }
+    struct stat st;
+    stat(filename, &st);
+    if (S_ISDIR(st.st_mode)) {
+	DIR *dir = opendir(filename);
+	struct dirent *entry;
+	char *jpg = ".jpg";
+        //iteration all jpg file
+	while ((entry = readdir(dir)) != 0) {
+	    if (!entry)
+		break;
+	    if (strstr(entry->d_name, jpg)) {
+		char buff[250];
+		memset(buff, 0, sizeof(buff));
+		strcpy(buff, filename);
+		strcat(buff, "/");
+		strcat(buff, entry->d_name);
+		printf("-------detect %s-------\n", buff);
+                test_detector_image(buff, outfile, net, thresh, hier_thresh, names,
+			alphabet);
+	    }
 
-        free_image(im);
-        free_image(sized);
-        if (filename) break;
-    }
+	}
+        //close dir handle
+	closedir(dir);
+    }else{
+     printf("-------detect %s-------\n", filename);
+     test_detector_image(filename, outfile, net, thresh, hier_thresh, names,
+			alphabet);
+     }
+
+
+
 }
+
+
 
 /*
 void censor_detector(char *datacfg, char *cfgfile, char *weightfile, int cam_index, const char *filename, int class, float thresh, int skip)
